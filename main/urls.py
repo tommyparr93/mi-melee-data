e602e3dfda9046c5c5ce82e9d79abcc70f3872ee
tommyparr93 <tommyparr93@gmail.com>
2023-04-29 22:26:45 -0400

    tournament list and pages, calculated fields on player detail

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PlayerListView.as_view(), name='players'),
+    path('', views.PlayerListView.as_view(), name='home'),
     # path('<int:pk>/', player_detail, name='player_detail'),
+    path('player', views.PlayerListView.as_view(), name='players'),
     path('<int:pk>/', views.PlayerDetailView.as_view(), name='player_detail'),
-    path('tournaments', views.PlayerListView.as_view(), name='tournaments'),
+    path('tournaments', views.TournamentListView.as_view(), name='tournaments'),
+    path('tournaments/<int:pk>', views.TournamentDetailView.as_view(), name='tournament_details'),
     path('regions', views.PlayerListView.as_view(), name='regions'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from .models import Player, Set
+from .models import Player, Set, Tournament, TournamentResults
 from django.views import generic
 from django.db.models import Q
 from django.views import View
@@ -13,15 +13,32 @@ def players(request):
     return HttpResponse("Hello world!")
 
 
+# do better lol
+def player_detail_calculations(player, sets):
+    wins = sets.filter(winnerid=player.playerid).count()
+    losses = sets.exclude(winnerid=player.playerid).count()
+    wr = (wins/sets.count())*100
+    return [wins, losses, int(wr)]
+
+
 class PlayerListView(generic.ListView):
     model = Player
-    template_name = 'main/index.html'  # Default: <app_label>/<model_name>_list.html
-    context_object_name = 'users'  # Default: object_list
+    template_name = 'main/players.html'
+    context_object_name = 'users'
     paginate_by = 25
     ordering = ['name']
-    queryset = Player.objects.all()  # Default: Model.objects.all()
+    queryset = Player.objects.all()
 
 
+class TournamentListView(generic.ListView):
+    model = Tournament
+    template_name = 'main/tournaments.html'
+    context_object_name = 'tournaments'
+    paginate_by = 25
+    ordering = ['-date']
+    queryset = Tournament.objects.all()
+
+# need to refactor have logic on this front to pass primary player and "opponent" within the sets context
 class PlayerDetailView(DetailView):
     model = Player
     template_name = 'main/player_detail.html'
@@ -31,15 +48,25 @@ class PlayerDetailView(DetailView):
         player = self.get_object()
         sets = Set.objects.filter(Q(player1=player) | Q(player2=player))
         context['sets'] = sets
+        # below is temporary --- remove when better solution is found to organize tournament info
+        tournaments = Tournament.objects.filter(set__in=sets).distinct()
+        context['tournaments'] = tournaments
+        context['calculations'] = player_detail_calculations(player, sets)
+
         return context
 
+class TournamentDetailView(DetailView):
+    model = Tournament
+    template_name = 'main/tournament_detail.html'
 
-class PlayerDetailView2(generic.ListView):
-    model = Player
-    template_name = 'main/index.html'  # Default: <app_label>/<model_name>_list.html
-    context_object_name = 'users'  # Default: object_list
-    paginate_by = 25
-    ordering = ['name']
-    queryset = Player.objects.all()  # Default: Model.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tournament = self.get_object()
+        sets = Set.objects.filter(tour=tournament.tour_id)
+        context['sets'] = sets
+        results = TournamentResults.objects.filter(tour=tournament.tour_id)
+        context['results'] = results
+        return context
+
 
 
