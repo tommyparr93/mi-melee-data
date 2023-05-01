4fddc365c1013848a4dd5393671390f017ccda98
tommyparr93 <tommyparr93@gmail.com>
2023-05-01 16:36:43 -0400

    added h2h in player details, updated admin player page, fixed urls, small fixes elsewhere

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,19 @@
-from django.contrib import admin
 from .models import Player, Set, Tournament, Region
+from django.contrib import admin
 
 
 # Register your models here.
-admin.site.register(Player)
+
 admin.site.register(Set)
 admin.site.register(Tournament)
 admin.site.register(Region)
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region_code')
+    list_filter = ('region_code',)
+    list_per_page = 50
+    search_fields = ('name',)
+
+
+admin.site.register(Player, PlayerAdmin)
--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 from django.views import View
 from django.db.models import Value, Case, Sum, IntegerField, When
 from django.views.generic.detail import DetailView
+from itertools import chain
 
 
 def players(request):
@@ -18,7 +19,37 @@ def player_detail_calculations(player, sets):
     wins = sets.filter(winnerid=player.playerid).count()
     losses = sets.exclude(winnerid=player.playerid).count()
     wr = (wins/sets.count())*100
-    return [wins, losses, int(wr)]
+    num_tournaments = sets.values('tour').distinct().count()
+    return [wins, losses, int(wr), num_tournaments]
+
+
+# it works but slow, need to speed up
+def get_head_to_head_results(player, sets):
+
+    l1 = sets.exclude(player1=player.playerid).values_list('player1_id', flat=True).distinct()
+    l2 = sets.exclude(player2=player.playerid).values_list('player2_id', flat=True).distinct()
+    opponents = set(l1) | set(l2)
+    opponents_queryset = Player.objects.filter(playerid__in=opponents).order_by('name')
+
+    opponent_records = []
+    for opponent in opponents_queryset:
+        matches = sets.filter(Q(player1=opponent) | Q(player2=opponent))
+        wins = 0
+        losses = 0
+        for match in matches:
+            if match.winnerid == player.playerid:
+                wins += 1
+            else:
+                losses += 1
+        wr = (wins / matches.count()) * 100
+        opponent_record = {
+            'name': opponent.name,
+            'wins': wins,
+            'losses': losses,
+            'win_rate': int(wr),
+        }
+        opponent_records.append(opponent_record)
+    return opponent_records
 
 
 class PlayerListView(generic.ListView):
@@ -48,9 +79,11 @@ class PlayerDetailView(DetailView):
         player = self.get_object()
         sets = Set.objects.filter(Q(player1=player) | Q(player2=player))
         context['sets'] = sets
+        context['opponents'] = get_head_to_head_results(player, sets)
         # below is temporary --- remove when better solution is found to organize tournament info
-        tournaments = Tournament.objects.filter(set__in=sets).distinct()
-        context['tournaments'] = tournaments
+        # tournaments = Tournament.objects.filter(set__in=sets).distinct()
+        # context['tournaments'] = tournaments
+
         context['calculations'] = player_detail_calculations(player, sets)
 
         return context
